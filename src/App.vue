76c53ca972af1af8753475c56d09a5c707cd29ac
forbes <afolabi2245@gmail.com>
2021-01-07 21:47:35 +0100

    added

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue';
+
+import '@/assets/css/tailwind.css';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Main
   }
-}
+};
+
 </script>
 
 <style>
---
+++ src/components/Main.vue
@@ -0,0 +1,6 @@
+<template>
+        <div class="bg-purple text-white sm:bg-green md:bg-blue md:text-yellow lg:bg-red xl:bg-orange ...">
+  Test
+</div>
+  
+</template>
